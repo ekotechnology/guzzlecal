18263494ac8b9081d4be32b0bc30fe7e1f2bd64f
Trea Hauet <trea@treahauet.com>
2013-01-14 02:58:20 -0700

    Initial Workbench package

---
+++ src/Ekotechnology/Guzzlecal/GuzzlecalServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Ekotechnology\Guzzlecal;
+
+use Illuminate\Support\ServiceProvider;
+
+class GuzzlecalServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('ekotechnology/guzzlecal');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
